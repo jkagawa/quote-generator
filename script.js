3a526ee9101ec6edca264887c1fe40a9f3f50fd5
Joshua Kagawa <joshuakagawa@gmail.com>
2021-05-15 23:56:48 -0400

    Style page elements, pull quotes from a JSON source using JS, add Twitter button functionality

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-console.log('testing');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+//Get Quotes from API
+let apiQuotes = [];
+
+//Show New Quote
+function newQuote() {
+  //Pick a random quote from apiQuotes array
+  var randnum = Math.floor(Math.random() * apiQuotes.length);
+  const quote = apiQuotes[randnum];
+
+  //Check Quote length to determine styling
+  if(quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  quoteText.textContent = quote.text;
+
+  //Check if author field is blank and replace with "Unknown"
+  if(!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+}
+
+async function getQuotes() {
+  const apiUrl = 'https://type.fit/api/quotes';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch(error) {
+    //Catch Error Here
+  }
+}
+
+//Tweet quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+//Event listener
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+//On Load
+getQuotes();
